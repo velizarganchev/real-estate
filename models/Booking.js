2520942a2396bb25366b0baf1f2c338ead913df5
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-02-05 19:38:55 +0100

    add BookingShema

---
+++ models/Booking.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const BookingShema = new mongoose.Schema({
+    place: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    checkIn: {
+        type: Date,
+        required: true,
+    },
+    checkout: {
+        type: Date,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    numberOfGuests: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+})
+
+export default mongoose.models.Booking || mongoose.model("Booking", BookingShema)
