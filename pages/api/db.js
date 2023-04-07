7c46cc356c2b58f8460c587d8b08eb287ead4991
Velizar Ganchev <velizar_ganchev@hotmail.com>
2023-04-07 13:37:55 +0200

    create database

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,25 +1,25 @@
-import mongodb from "../../utils/mongodb"
+//import mongodb from "../../utils/mongodb"
 
-export default function handler(req, res) {
-  mongodb.dbConnect();
-  mongodb.dbDisconnect();
-  res.status(200).json({ name: 'John Doe' })
-}
-
-// import mongodb from "../../utils/mongodb"
-// import jsondb from "../../jsondb/products"
-// import Product from "../../models/Product";
-
-
-// export default async function handler(req, res) {
-//   await mongodb.dbConnect();
-
-//   await Product.deleteMany();
-//   await Product.insertMany(jsondb.products);
-//   const products = await Product.find({})
-
-//   await mongodb.dbDisconnect();
-
-//   //res.send({ text: 'Data saved' })
-//   res.send(products);
+// export default function handler(req, res) {
+//   mongodb.dbConnect();
+//   mongodb.dbDisconnect();
+//   res.status(200).json({ name: 'John Doe' })
 // }
+
+import mongodb from "../../utils/mongodb"
+import jsondb from "../../jsondb/places"
+import Places from "../../models/Place";
+
+
+export default async function handler(req, res) {
+  await mongodb.dbConnect();
+
+  await Places.deleteMany();
+  await Places.insertMany(jsondb.places);
+  const places = await Places.find({})
+
+  await mongodb.dbDisconnect();
+
+  //res.send({ text: 'Data saved' })
+  res.send(places);
+}
